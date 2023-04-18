6c8bcc627b14eab335607009f2f3966785af9cca
Bole Chen <avenger@php.net>
2023-04-18 20:30:48 +0800

    style: wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,9 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
+<<<<<<< HEAD
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
+  title: "ChatGPT Pro",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
@@ -18,7 +19,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ChatGPT Pro",
     statusBarStyle: "default",
   },
 };
